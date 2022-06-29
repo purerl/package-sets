bd4943f7ebb33d81b3df03a49882178fe22520b3
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2022-06-29 09:06:17 +0000

    Bump versions

--- shell.nix
+++ shell.nix
@@ -10,25 +10,24 @@ let
     builtins.fetchGit {
       url = "https://github.com/purerl/nixpkgs-purerl.git";
       ref = "master";
-      rev = "0ff4c54219fe60c787334051f3303bdc8ba63e9d";
+      rev = "7eadeb83eb2590039c96386d572db3a2fce19370";
     };
-
-  purerlSupport =
-    builtins.fetchGit {
-      name = "purerl-support-packages";
-      url = "https://github.com/id3as/nixpkgs-purerl-support.git";
-      rev = "f86be701bf1828c75d81b24428907a6ac9d7dad9";
-    };
-
 
   nixpkgs =
     import pinnedNix {
       overlays = [
         (import purerlReleases)
-        (import purerlSupport)
       ];
     };
 
+  easy-ps = import
+    (nixpkgs.pkgs.fetchFromGitHub {
+      ## not merged yet for 0.15.3 https://github.com/justinwoo/easy-purescript-nix/pull/210
+      owner = "toastal";
+      repo = "easy-purescript-nix";
+      rev = "0cd6f267ac790dbe5aaa68c68c074d233ff21e2c";
+      sha256 = "19111dmlz6i4f4v2crfi8b86nfjfag5fg2gxdyx4310nc153qcb3";
+    }) { pkgs = nixpkgs; };
 in
 
 with nixpkgs;
@@ -37,11 +36,11 @@ mkShell {
   buildInputs = with pkgs; [
     # Purescript - we use a specific version rather than
     # whatever the latest is exposed via nixpkgs
-    purerl-support.purescript-0-14-5
-    purerl-support.spago-0-20-3
+    easy-ps.purs-0_15_3
+    easy-ps.spago
 
     # Purerl backend for purescript
-    purerl.purerl-0-0-14
+    purerl.purerl-0-0-17
 
     dhall
     dhall-json
